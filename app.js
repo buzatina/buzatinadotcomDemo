84d50d45619cead0f4ab85223acde7c52c4202c0
buzatina <tlntlatleng@gmail.com>
2017-05-28 21:51:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // create http server
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-server.listen(process.env.PORT || 3000);
+server.listen(2800);
 
 // Set the View Engine
 app.set('views', path.join(__dirname, 'views'));
